4097d740d5560931f26d2ce37429911806174803
DmitryVesenniy <press8328@protonmail.com>
2021-01-15 09:20:20 +0700

    refactor: изменить имя файла create_documents.sql на create.sql, добавить новые таблицы, создать insert_settingsApp.sql, migrate.sh
    sudo bash ./migrate.sh

--- backend/deploy/psql/create.sql
+++ backend/deploy/psql/create.sql
@@ -20,5 +20,27 @@ CREATE TABLE IF NOT EXISTS comments (
     FOREIGN KEY ("cityId") REFERENCES "cityLocs"(Id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS settingapps (
+    Id SERIAL,
+    "fieldName" VARCHAR(100) PRIMARY KEY,
+    description VARCHAR(200) NULL,
+    value VARCHAR(500) NULL,
+    system BOOLEAN DEFAULT False,
+    enable BOOLEAN DEFAULT True,
+    "createdAt" timestamp with time zone NULL,
+    "updatedAt" timestamp with time zone NULL
+);
+
+CREATE TABLE IF NOT EXISTS contents (
+    Id SERIAL,
+    "componentName" VARCHAR(100) PRIMARY KEY,
+    content VARCHAR(1000),
+    header VARCHAR(500) NULL,
+    enable BOOLEAN DEFAULT True,
+    "createdAt" timestamp with time zone NULL,
+    "updatedAt" timestamp with time zone NULL
+);
+
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public to postgres;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO postgres;
+
