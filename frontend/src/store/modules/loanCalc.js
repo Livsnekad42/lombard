05552bc03284a30cac915363a404f28fed371e12
Livsnekad42 <savelevartem5@gmail.com>
2021-01-20 12:22:33 +0700

    change settings in calculator, change closed data to public data

--- frontend/src/store/modules/loanCalc.js
+++ frontend/src/store/modules/loanCalc.js
@@ -1,6 +1,6 @@
 import CalculateService from "../../app/services/CalculateService";
 import { goldProbes } from "../../_config";
-import {editSetting, getSettings} from "@/app/api-admin";
+import {editSetting, getPublicSettings } from "@/app/api-admin";
 
 
 const state = {
@@ -10,7 +10,7 @@ const state = {
   term: "5",
   amount: "",
   returnedAmount: "",
-  percentLoanCalc : '',
+  percentLoanCalc : 0.2,
   probePrice: {
     "585": '',
     "750": ''
@@ -47,16 +47,16 @@ const mutations = {
 
 const actions = {
    async actualizeCalcPrices(ctx) {
-     let gold585 = {
+     const gold585 = {
        type: '585',
-       data: ''
+       data: 10500
      };
-     let gold750 = {
+     const gold750 = {
        type: '750',
-       data: ''
+       data: 13461
      };
-     let response =  await new Promise((resolve, reject) => {
-       getSettings()
+     const response =  await new Promise((resolve, reject) => {
+       getPublicSettings()
            .then(res => {
              if (res.data.err) reject(res);
              resolve(res);
@@ -65,12 +65,18 @@ const actions = {
              reject(err);
            });
      });
-     gold585.data = response.data.find(elem => elem.fieldName == 'probePrice_585').value;
-     gold750.data = response.data.find(elem => elem.fieldName == 'probePrice_750').value;
+     const response585 = response.data.find(elem => elem.fieldName == 'probePrice_585').value;
+     const response750 = response.data.find(elem => elem.fieldName == 'probePrice_750').value;
+     if (response585) {
+       gold585.data = response585;
+     };
+     if (response750) {
+       gold750.data = response750;
+     };
 
      ctx.commit('setCalcProbePrice', {type: gold585.type, data: gold585.data});
      ctx.commit('setCalcProbePrice', {type: gold750.type, data: gold750.data});
-     ctx.commit('setPercentLoanCalc', response.data.find(elem => elem.fieldName == 'processingPercent').value)
+  //   ctx.commit('setPercentLoanCalc', response.data.find(elem => elem.fieldName == 'processingPercent').value)
   },
   startCalculate({ state }) {
     const CALCULATE = new CalculateService(state.percentLoanCalc);
@@ -82,7 +88,7 @@ const actions = {
     state.returnedAmount = CALCULATE.returnedAmount;
   },
   async setSetting(ctx, data) {
-    let newSetting = await new Promise((resolve, reject) => {
+    const newSetting = await new Promise((resolve, reject) => {
       editSetting(data)
           .then(res => {
             if (res.data.err) reject(res);
@@ -92,12 +98,17 @@ const actions = {
             reject(err);
           });
     });
-    if (newSetting.data.setting.fieldName == 'probePrice_585') {
-      ctx.commit('setCalcProbePrice', {type: '585', data: newSetting.data.setting.value});
-    } else if (newSetting.data.setting.fieldName == 'probePrice_750') {
-      ctx.commit('setCalcProbePrice', {type: '750', data: newSetting.data.setting.value});
-    } else if (newSetting.data.setting.fieldName == 'processingPercent') {
-      ctx.commit('setPercentLoanCalc', newSetting.data.setting.value);
+    switch (newSetting.data.setting.fieldName) {
+      case 'probePrice_585':
+        ctx.commit('setCalcProbePrice', {type: '585', data: newSetting.data.setting.value});
+        break;
+      case 'probePrice_750':
+        ctx.commit('setCalcProbePrice', {type: '750', data: newSetting.data.setting.value});
+        break;
+ /*     case  'processingPercent':
+        ctx.commit('setPercentLoanCalc', newSetting.data.setting.value);
+        break;
+  */
     }
   }
 };
