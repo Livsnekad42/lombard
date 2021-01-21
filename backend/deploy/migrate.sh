0c4c4586af04dd6fe1a21428806305e1f12e848d
DmitryVesenniy <press8328@protonmail.com>
2021-01-21 23:39:49 +0700

    fix: изменить user db

--- backend/deploy/migrate.sh
+++ backend/deploy/migrate.sh
@@ -1,2 +1,2 @@
-psql -h localhost -d tezdb -U localtez -p 5432 -a -q -f ./psql/create.sql
-psql -h localhost -d tezdb -U localtez -p 5432 -a -q -f ./psql/insert_settingsApp.sql
+psql -h localhost -d tezdb -U postgres -p 5432 -a -q -f ./psql/create.sql
+psql -h localhost -d tezdb -U postgres -p 5432 -a -q -f ./psql/insert_settingsApp.sql
--- backend/deploy/psql/insert_settingsApp.sql
+++ backend/deploy/psql/insert_settingsApp.sql
@@ -2,7 +2,7 @@ DELETE FROM public.settingapps;
 
 INSERT INTO public.settingapps ("fieldName", description, value, system, "enable", "isPublic") VALUES
 ('prolongationState', 'Состояние функционала пролонгации', '', '1', '1', '0'),
-('processingPercent', 'Процент процессинга 0.10 - 10%', '0.05', '1', '1', '0'),
+('processingPercent', 'Процент процессинга %', '-', '1', '1', '0'),
 ('probePrice_585', 'Стоимость 585 пробы за один грамм', '10500', '1', '1', '1'),
 ('probePrice_750', 'Стоимость 750 пробы за один грамм', '13461', '1', '1', '1');
 
